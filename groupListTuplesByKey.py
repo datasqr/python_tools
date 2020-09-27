e724c549c1bf8038ac3451d7818d1d28f8acf3a6
Mateusz Zbikowski <mateusz.zbikowski@gmail.com>
2020-09-27 10:16:29 +0200

    Update groupListTuplesByKey.py

--- groupListTuplesByKey.py
+++ groupListTuplesByKey.py
@@ -2,6 +2,7 @@ import itertools
 import operator
 
 def groupListTuplesByKey(list):
+    list = sorted(list, key=lambda tup: tup[0])
     it = itertools.groupby(list, operator.itemgetter(0))
     for key, subiter in it:
         yield key, [item[1] for item in subiter]
